a72b7a79d8d591be02a5bf278ae69dfec8a274e8
ozerozdal <ozerozdal@gmail.com>
2018-08-26 22:47:58 -0400

    Update

--- MCMC.py
+++ MCMC.py
@@ -1,11 +1,19 @@
 from classMCMC import *
+import os
 
-PathForFreeParams = "/Users/oozdal/readclass/freeparams.txt"
-LesHouchesFilePath = "/Users/oozdal/readclass/LesHouches.in.LRSM"
+PathForFreeParams = os.path.join("/Users/oozdal/readclass/", "freeparams.txt")
+LesHouchesFileFullPath = os.path.join("/Users/oozdal/readclass/", "LesHouches.in.LRSM")
+UpdatedLesHouchesFileFullPath = os.path.join(LesHouchesFileFullPath)+"_updated"
+SPhenoExecFullPath = os.path.join("/Users/oozdal/hepwork/SPheno-3.3.8/bin/", "SPhenoLRSM_NonSUSY_CKMRgR_tsbar")
+SPhenoOutputName = "Spheno.spc"
 
 ###################################################################
 
+OutputNumber = 0 
+MaxNumberOfOutput = 100
+
 Monte = MCMC(PathForFreeParams)
 
-if Monte.CheckLesHouches(LesHouchesFilePath) == True:
-    Monte.AssignValues()
+while OutputNumber < MaxNumberOfOutput:
+    Monte.AssignValues(LesHouchesFileFullPath, UpdatedLesHouchesFileFullPath)
+    Monte.RunSPheno(SPhenoExecFullPath)
--- classMCMC.py
+++ classMCMC.py
@@ -13,32 +13,52 @@ class MCMC():
         for i in range(self.numberofparameters):
             self.params.update({self.freeparamsdata["ParamName"][i] : np.random.uniform(self.freeparamsdata["LowerLimit"][i], self.freeparamsdata["UpperLimit"][i]) }) 
 
-    def CheckLesHouches(self,LesHouchesFullPath):
-        self.LesHouchesFullPath = LesHouchesFullPath
-        self.SplitFilePath = self.LesHouchesFullPath.split("/") 
-        self.LesHouchesFileName = self.SplitFilePath[-1]
-        
+    def CheckFile(self, FullFilePath):
+        self.FullFilePath = FullFilePath
+        self.SplitFilePath = self.FullFilePath.split("/")
+        self.FileName = self.SplitFilePath[-1]
+
         self.FileElement_list = []
         for i in range(1, len(self.SplitFilePath)-1):
             self.FileElement_list.append("/"+self.SplitFilePath[i])
 
-        self.LesHouchesOnlyPath = ""
+        self.OnlyFilePath = OnlyFilePath = ""
         for i in range(len(self.FileElement_list)):
-            self.LesHouchesOnlyPath = self.LesHouchesOnlyPath + self.FileElement_list[i]
+            self.OnlyFilePath = self.OnlyFilePath + self.FileElement_list[i]
 
-        return os.path.isfile(self.LesHouchesFullPath)
+        return os.path.isfile(self.FullFilePath)    
 
-    def AssignValues(self):
-        with open(self.LesHouchesFullPath, 'r') as file:
-            self.filedata = file.read()
 
-        # Replace the target string
-        for i in range(self.numberofparameters):
-            self.filedata = self.filedata.replace(self.freeparamsdata["ParamName"][i], str("{:.6E}".format(self.params[str(self.freeparamsdata["ParamName"][i])])))
+    def AssignValues(self, LesHouchesFileFullPath, UpdatedLesHouchesFileFullPath):
+        self.LesHouchesFileFullPath = LesHouchesFileFullPath
+        self.UpdatedLesHouchesFileFullPath = UpdatedLesHouchesFileFullPath
 
-        # Write the file out again
-        with open(self.LesHouchesFullPath+"_updated", 'w') as updatedfile:
-            self.filedata = updatedfile.write(self.filedata)
+        if self.CheckFile(self.LesHouchesFileFullPath) == True:
+            with open(self.LesHouchesFileFullPath, 'r') as file:
+                self.filedata = file.read()
+
+            # Replace the target string
+            for i in range(self.numberofparameters):
+                self.filedata = self.filedata.replace(self.freeparamsdata["ParamName"][i], str("{:.6E}".format(self.params[str(self.freeparamsdata["ParamName"][i])])))
+
+            # Write the file out again
+            with open(self.UpdatedLesHouchesFileFullPath, 'w') as updatedfile:
+                self.filedata = updatedfile.write(self.filedata)                        
+        else:
+            print "Error in AssignValues: No LesHouches File is found in the given path!"
+
+    def RunSPheno(self, SPhenoExeFullPath, LesHouchesInputFullPath = None):
+        self.SPhenoExeFullPath = SPhenoExeFullPath
+        self.LesHouchesInputFullPath =  LesHouchesInputFullPath
+
+        if self.LesHouchesInputFullPath == None and os.path.isfile(self.UpdatedLesHouchesFileFullPath) == True:
+            os.system(str(self.SPhenoExeFullPath)+" "+str(self.UpdatedLesHouchesFileFullPath))
+        elif self.LesHouchesInputFullPath != None and os.path.isfile(self.LesHouchesInputFullPath) == True:
+            os.system(str(self.SPhenoExeFullPath)+" "+str(self.LesHouchesInputFullPath))
+        else:
+            print "Error in RunSPheno: No LesHouches File is found in the given path!"
+
+
 
 #    def CopySLHAtoCards(self, SLHAPath, CardsPath):
 #        shutil.copy2(SLHAPath, CardsPath + "/param_card.dat") # complete target filename given
